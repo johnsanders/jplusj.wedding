7ab93bf94754fc3946f55806d632107abf002606
John Sanders <john.sanders@turner.com>
2017-08-13 15:06:18 +0100

    Fix nav header.  Not sure if flexbox+float is such a great thing

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -8,12 +8,17 @@ class Nav extends React.Component {
 	render(){
 		return (
 
-			<nav className="nav nav-pills fixed-top justify-content-end">
-				<Link to="/" className="nav-link active">Home</Link>
-				<Link to="/" className="nav-link">About Us</Link>
-				<Link to="/" className="nav-link">Gallery</Link>
-				<Link to="/" className="nav-link">Wedding</Link>
-				<Link to="/" className="nav-link">RSVP!</Link>
+			<nav className="nav nav-pills fixed-top">
+				<div className="container" style={{justifyContent:'space-between'}}>
+					<div className="navbar-brand">JplusJ.wedding</div>
+					<div className="nav nav-pills" style={{float:'right'}}>
+						<Link to="/" className="nav-link active">Home</Link>
+						<Link to="/" className="nav-link">About Us</Link>
+						<Link to="/" className="nav-link">Gallery</Link>
+						<Link to="/" className="nav-link">Wedding</Link>
+						<Link to="/" className="nav-link">RSVP!</Link>
+					</div>
+				</div>
 			</nav>
 		);
 	}
